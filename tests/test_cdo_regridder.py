91664b552c21b1350cd3007d9ab34c2f6d82a023
Pingu Carsti <ehbrecht@dkrz.de>
2018-10-22 15:33:20 +0200

    cleaned up test

--- tests/test_cdo_regridder.py
+++ tests/test_cdo_regridder.py
@@ -1,16 +1,9 @@
 from regridder.cdo_regridder import regrid
 
+CMIP5_NC = "/opt/data/cmip5/tas_day_HadGEM2-ES_historical_r1i1p1_19791201-19891130.nc"
+CORDEX_NC = "/opt/data/cordex/tasmin_AFR-44i_ECMWF-ERAINT_evaluation_r1i1p1_MOHC-HadRM3P_v1_mon_199001-199012.nc"
+
 
 def test_regrid():
-    paths = {
-        "global": [
-            "/opt/data/cmip5/tas_day_HadGEM2-ES_historical_r1i1p1_19791201-19891130.nc"
-        ],
-        "regional": [
-            "/opt/data/cordex/tasmin_AFR-44i_ECMWF-ERAINT_evaluation_r1i1p1_MOHC-HadRM3P_v1_mon_199001-199012.nc"
-        ]
-    }
-
-    for domain_type, paths in paths.items():
-        for path in paths:
-            regrid(path, domain_type)
+    regrid(CORDEX_NC, 'regional')
+    # regrid(CMIP5_NC, 'global')
